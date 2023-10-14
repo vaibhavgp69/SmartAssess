2f6609a5f1ee42d39ec92aae7cee8e8fc6eeadb7
Niranjan S <111201410+Ninja2k3@users.noreply.github.com>
2023-10-14 13:01:01 +0530

    com

--- SmartAssess/settings.py
+++ SmartAssess/settings.py
@@ -41,15 +41,19 @@ INSTALLED_APPS = [
     "api.apps.ApiConfig",
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'djoser',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
 
 ]
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
