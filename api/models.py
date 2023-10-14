23166502796204dfeb727161c3704242f1a629e2
Vaibhav GP <112196928+vaibhavgp69@users.noreply.github.com>
2023-10-15 04:49:14 +0530

    stashed

--- api/models.py
+++ api/models.py
@@ -26,12 +26,12 @@ class Mcq(models.Model):
     assessment_id =  models.CharField(max_length=100, default = "")
     assesment = models.ForeignKey(Assessment, on_delete=models.CASCADE)
     gen_question = models.CharField(max_length=2000, default = "")
-    is_correct = models.BooleanField()
-    time_taken  = models.IntegerField()
-    difficulty_score =  models.DecimalField(max_digits=4, decimal_places=2)
+    is_correct = models.BooleanField(null = True , default = "")
+    time_taken  = models.IntegerField(null = True , default = "")
+    difficulty_score =  models.DecimalField(max_digits=4, decimal_places=2,null = True , default = "")
 
     def __str__(self):
-        return self.difficulty_score + " : Topic : " + self.assesment.topic  + " question - " + ast.literal_eval(self.gen_question)["question"] 
+        return str(self.difficulty_score) + " : Topic : " + self.assesment.topic  + " question - " + ast.literal_eval(self.gen_question)["question"] 
     
 
     
