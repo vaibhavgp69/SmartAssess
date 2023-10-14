49541d5962edd538de361bc4e7a61860059903be
Vaibhav GP <112196928+vaibhavgp69@users.noreply.github.com>
2023-10-14 11:07:39 +0530

    del

--- SmartAssess/settings.py
+++ SmartAssess/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api.apps.ApiConfig",
-    "frontend.apps.FrontendConfig",
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
