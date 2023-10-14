dad644e142613b0b1840796762b7f3bd9c5b648c
Niranjan S <111201410+Ninja2k3@users.noreply.github.com>
2023-10-15 02:14:57 +0530

    a

--- frontend/src/components/PDF.js
+++ frontend/src/components/PDF.js
@@ -1,28 +1,3 @@
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { pdfjs } from 'react-pdf';
 
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
-  }
-});
-
-// Create Document Component
-export const MyDocument = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-);
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
